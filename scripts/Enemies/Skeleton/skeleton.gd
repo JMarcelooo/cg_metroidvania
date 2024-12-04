2a290726c948674c6bcc3dcb67f9b7482fcef588
Manu Gaviper <jmarcelooocontato@gmail.com>
2024-12-04 16:59:21 -0300

    criação de inimigos e criação do sprite do jogador

--- scripts/Enemies/Skeleton/skeleton.gd
+++ scripts/Enemies/Skeleton/skeleton.gd
@@ -1,16 +1,55 @@
-extends RigidBody2D
+extends CharacterBody2D
+
+var SPEED = 60
+@export var time = 1.5
+const JUMP_FORCE = -400
+@export var gravity = 30
+var facing_right = true
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var flip_timer = $Timer
+	flip_timer.start(time)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		$AnimatedSprite2D.play("Walking")
+	
+	velocity.x = SPEED
+	move_and_slide()
 
 
-func _on_hitbox_area_entered(area: Area2D) -> void:
+func flip():
+	facing_right = !facing_right
+	scale.x = abs(scale.x) * -1
+	if facing_right:
+		SPEED = abs(SPEED)
+	else:
+		SPEED = abs(SPEED) * -1
+
+
+func _on_timer_timeout() -> void:
+	flip()
+	$Timer.start(time)
+
+
+
+func _on_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	if area.name == "sword":
+		$AnimatedSprite2D.play("Dead")
+		SPEED = 0
+		await $AnimatedSprite2D.animation_finished
 		queue_free()
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		print("colisão")
+		var animation = body.get_node("AnimationPlayer")
+		if animation:
+			animation.play("Dead")
+			await animation.animation_finished
+			queue_free()
